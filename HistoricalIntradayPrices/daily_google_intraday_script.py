ecd53d2e363b573820062b5d5aafd1049ea91922
maupardh <maupardh@gmail.com>
2015-08-24 21:11:06 -0400

    std from PyCharm

--- HistoricalIntradayPrices/daily_google_intraday_script.py
+++ HistoricalIntradayPrices/daily_google_intraday_script.py
@@ -17,8 +17,9 @@ def run():
     if datetime.date.today().weekday() >= 5:
         return 0
 
+    # Stocks
     log_file_path = \
-        os.path.join('/Users/hmaupard/Documents/FinancialData/US/Equities/Google/',
+        os.path.join('/Users/hmaupard/Documents/FinancialData/US/Equities/Logs/',
                      datetime.date.today().isoformat()+"-GoogleImport.txt")
     my_logging.initialize_logging(log_file_path)
 
@@ -34,16 +35,19 @@ def run():
 
     my_logging.shutdown()
 
+    # ETFs
     etf_universe = sorted(list(set(
         my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/ETFs/Universes/ETFUniverse.csv'))))
     log_file_path = \
-        os.path.join('/Users/hmaupard/Documents/FinancialData/US/ETFs/Google/',
-                     datetime.date.today().isoformat()+"-GoogleImport.txt")
+        os.path.join('/Users/hmaupard/Documents/FinancialData/US/ETFs/Logs/',
+                     datetime.date.today().isoformat() + "-GoogleImport.txt")
     my_logging.initialize_logging(log_file_path)
 
     google_intraday_import.retrieve_and_store_today_price(etf_universe,
                                                           '/Users/hmaupard/Documents/FinancialData/US/ETFs/Google/')
 
     return 0
+
+
 
 run()
--- HistoricalIntradayPrices/main.py
+++ HistoricalIntradayPrices/main.py
@@ -14,23 +14,25 @@ def main():
     if datetime.date.today().weekday() >= 5:
         return 0
 
-    # log_file_path = \
-    #     os.path.join('/Users/hmaupard/Documents/FinancialData/US/Equities/Google/',
-    #                  datetime.date.today().isoformat()+"-GoogleImport.txt")
-    # my_logging.initialize_logging(log_file_path)
-    #
-    # stock_universe = \
-    #     my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/SPY.csv') + \
-    #     my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/MDY.csv') + \
-    #     my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/IWV.csv') + \
-    #     my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/QQQ.csv')
-    # stock_universe = sorted(list(set(stock_universe)))
-    #
-    # google_intraday_import.retrieve_and_store_today_price(stock_universe,
-    #                                                       '/Users/hmaupard/Documents/FinancialData/US/Equities/Google/')
-    #
-    # my_logging.shutdown()
+    # Stocks
+    log_file_path = \
+        os.path.join('/Users/hmaupard/Documents/FinancialData/US/Equities/Logs/',
+                     datetime.date.today().isoformat()+"-GoogleImport.txt")
+    my_logging.initialize_logging(log_file_path)
 
+    stock_universe = \
+        my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/SPY.csv') + \
+        my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/MDY.csv') + \
+        my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/IWV.csv') + \
+        my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/Equities/Universes/QQQ.csv')
+    stock_universe = sorted(list(set(stock_universe)))
+
+    google_intraday_import.retrieve_and_store_today_price(stock_universe,
+                                                          '/Users/hmaupard/Documents/FinancialData/US/Equities/Google/')
+
+    my_logging.shutdown()
+
+    # ETFs
     etf_universe = sorted(list(set(
         my_tools.read_csv_all_lines('/Users/hmaupard/Documents/FinancialData/US/ETFs/Universes/ETFUniverse.csv'))))
     log_file_path = \
