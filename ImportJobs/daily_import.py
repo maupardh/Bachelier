aacd972620fa8b7249f7c8f3d9c34fa32c332d6d
maupardh <maupardh@gmail.com>
2015-10-06 21:03:18 -0400

    'std'

--- ImportJobs/daily_import.py
+++ ImportJobs/daily_import.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('F:/pythonCode/Utilities')
+sys.path.append('F:/pythonCode/HistoricalAssets')
+sys.path.append('F:/pythonCode/HistoricalIntradayPrices')
+sys.path.append('F:/pythonCode/HistoricalExtradayPrices')
 import daily_asset_refresh_from_bbg_script
 import daily_intraday_equity_prices_import_script
 import datetime
