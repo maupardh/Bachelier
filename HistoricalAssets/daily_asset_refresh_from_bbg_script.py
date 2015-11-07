673f988dbb89e0b39d00f8222e7c90b8e33ce60a
maupardh <maupardh@gmail.com>
2015-11-07 12:23:50 -0500

    'werhy'

--- HistoricalAssets/daily_asset_refresh_from_bbg_script.py
+++ HistoricalAssets/daily_asset_refresh_from_bbg_script.py
@@ -1,9 +1,18 @@
 import Utilities.my_assets
+import os.path
+import datetime
+import Utilities.my_logging
 
 
 def refresh(date):
 
-    if date.isoweekday() >= 6:
-        return 0
+    log_file_path = \
+        os.path.join('F:/FinancialData/Logs/',
+                     date.isoformat(), "IntradayYahooEquityImport.txt")
+    Utilities.my_logging.initialize_logging(log_file_path)
 
     Utilities.my_assets.refresh_assets(date)
+
+    Utilities.my_logging.shutdown()
+
+refresh(datetime.date.today())
