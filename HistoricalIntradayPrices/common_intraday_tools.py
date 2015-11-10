5810c9c4bca40a787e567dd339bb0ff9bdfb3ebd
maupardh <maupardh@gmail.com>
2015-11-10 17:18:50 -0500

    'std commit'

--- HistoricalIntradayPrices/common_intraday_tools.py
+++ HistoricalIntradayPrices/common_intraday_tools.py
@@ -44,3 +44,5 @@ REINDEXES_CACHE = {}
 for country in Utilities.my_markets.COUNTRIES:
     REINDEXES_CACHE[country] = {
         datetime.date.today().isoformat(): get_standardized_intraday_equity_dtindex(country, datetime.date.today())}
+
+# local master branch
