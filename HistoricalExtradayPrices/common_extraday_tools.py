252a4a31a8833a6655c6b9b8aebfa9573d0d6f31
maupardh <maupardh@gmail.com>
2015-11-12 13:28:15 -0500

    'added asserts to everything except common extraday tools'

--- HistoricalExtradayPrices/common_extraday_tools.py
+++ HistoricalExtradayPrices/common_extraday_tools.py
@@ -12,20 +12,31 @@ __EXTRADAY_PRICES_DIRECTORY = os.path.join('F:/', 'financialData', 'HistoricalEx
 
 
 def get_standardized_extraday_equity_dtindex(country, start_date, end_date):
-
-    reg_idx = pd.bdate_range(start_date, end_date)
-    reg_idx.name = STANDARD_INDEX_NAME
-    holidays_idx = Utilities.my_holidays.HOLIDAYS_BY_COUNTRY_CONFIG.get(country, {})
-    reg_idx = reg_idx.difference(holidays_idx)
-    return reg_idx
+    try:
+        assert(isinstance(country, basestring) and isinstance(start_date, datetime.date)
+               and isinstance(end_date, datetime.date))
+        reg_idx = pd.bdate_range(start_date, end_date)
+        reg_idx.name = STANDARD_INDEX_NAME
+        holidays_idx = Utilities.my_holidays.HOLIDAYS_BY_COUNTRY_CONFIG.get(country, {})
+        reg_idx = reg_idx.difference(holidays_idx)
+        return reg_idx
+    except AssertionError:
+        logging.warning('Calling get_standardized_extraday_equity_dtindex with wrong argument types')
+    except Exception as err:
+        logging.warning('rget_standardized_extraday_equity_dtindex failed with message: %s' % err.message)
 
 
 def get_standardized_extraday_fx_dtindex(start_date, end_date):
-
-    reg_idx = pd.bdate_range(start_date, end_date)
-    reg_idx.name = STANDARD_INDEX_NAME
-    return reg_idx
-
+    try:
+        assert(isinstance(country, basestring) and isinstance(start_date, datetime.date)
+               and isinstance(end_date, datetime.date))
+        reg_idx = pd.bdate_range(start_date, end_date)
+        reg_idx.name = STANDARD_INDEX_NAME
+        return reg_idx
+    except AssertionError:
+        logging.warning('Calling get_standardized_extraday_fx_dtindex with wrong argument types')
+    except Exception as err:
+        logging.warning('get_standardized_extraday_fx_dtindex failed with message: %s' % err.message)
 
 REINDEXES_CACHE = {}
 
@@ -47,22 +58,25 @@ def _get_extraday_prices(date):
         content = content[STANDARD_COL_NAMES]
         logging.info('Reading successful')
         return content
-    except Exception, err:
+    except Exception as err:
         logging.warning('Reading failed with error: %s' % err.message)
         return pd.DataFrame(None)
 
 
 def get_extraday_prices(start_date, end_date):
-
-    content = pd.DataFrame(None)
-    for d in pd.date_range(start_date, end_date, freq='D'):
-        content = content.append(_get_extraday_prices(d.date()))
-    return content
+    try:
+        content = pd.concat(map(lambda d: _get_extraday_prices(d.date()), pd.date_range(start_date, end_date, freq='D')))
+        return content
+    except Exception as err:
+        logging.warning('get_extraday_prices failed with error: %s' % err.message)
+        return pd.DataFrame(None)
 
 
 def write_extraday_prices_table_for_single_day(new_content, date, resolve_method='R'):
 
     try:
+        assert(isinstance(new_content, pd.DataFrame) and isinstance(date, datetime.date)
+               and isinstance(resolve_method, basestring))
         logging.info('Retrieving existing prices')
         old_content = _get_extraday_prices(date)
 
@@ -86,7 +100,8 @@ def write_extraday_prices_table_for_single_day(new_content, date, resolve_method
         merged_content_resolved.index = merged_content_resolved[STANDARD_INDEX_NAME]
         merged_content_resolved = merged_content_resolved[STANDARD_COL_NAMES]
         Utilities.my_general_tools.store_and_log_pandas_df(_get_extraday_csv_zip_path(date), merged_content_resolved)
-
-    except Exception, err:
+    except AssertionError:
+        logging.warning('Calling write_extraday_prices_table_for_single_day with wrong argument types')
+    except Exception as err:
         logging.warning('Something went wrong during rewrite of date: %s, with message: %s'
                         % (date.isoformat(), err.message))
