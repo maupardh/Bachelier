f4dbfb4a55785d1bad3bc9ec19e1e64e74de6419
maupardh <maupardh@gmail.com>
2016-05-26 11:47:17 +0200

    inits

--- Strategies/Training/predict_returns.py
+++ Strategies/Training/predict_returns.py
@@ -28,14 +28,13 @@ def run():
 
     # Compute signals and train
     universe_start_date = datetime.date(2011, 1, 1)
-    universe_end_date = datetime.date(2011, 1, 1)
+    universe_end_date = datetime.date(2011, 6, 1)
     country = 'US'
 
     signals_df = Strategies.Targets.returns.compute_hedged_returns(
         assets_universe_with_factors, Utilities.datetime_tools.get_business_days(country,
                                                                                  universe_start_date,
                                                                                  universe_end_date))
-
 
 
     return
